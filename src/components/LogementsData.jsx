9e590c6d95a8d4bef7957c2b96e242c53eb87564
kevin santos <santoskevin27@yahoo.fr>
2025-03-26 14:42:26 +0100

    saving Home and Details changes

--- src/components/LogementsData.jsx
+++ src/components/LogementsData.jsx
@@ -2,7 +2,7 @@ import logements from '../data/logements.json';
 import Logement from './Logement'; //On importe le composant Logement
 
 const logementsData = () => {
-  //Creation d'une div, class et index pour chaque sous tableau
+  //Creation d'une div et classe pour chaque sous tableau
   return (
     <div className="logements-container">
       {logements.map((logement) => (
